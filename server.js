04c19ca8af40f453c229cbbf1f46a6e45d1328cc
amisra123 <aisuryamisra@gmail.com>
2021-06-30 21:43:32 +0530

    Connection to mongoDB cluster

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-const express = require("express")
+const express = require('express');
+const connectDB = require('./config/db');
+const app = express();
 
-const app = express()
+const PORT = process.env.PORT || 5000;
 
-const PORT = process.env.PORT || 5000
+//Connect to Database
+connectDB();
 
-app.get("/",(req,res) =>{
-    res.send("API running")
-})
+app.get('/', (req, res) => {
+  res.send('API running');
+});
 
-
-app.listen(PORT,() => {
-    console.log("Server is up and running ")
-})
+app.listen(PORT, () => {
+  console.log('Server is up and running ');
+});
