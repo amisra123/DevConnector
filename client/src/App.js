ff22393838fe5d9e190f1cbc15df0217e76b7a4a
amisra123 <aisuryamisra@gmail.com>
2021-07-15 21:51:10 +0530

    Landing page added

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+import NavBar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import './App.css';
+
+const App = () => {
+  return (
+    <Fragment>
+      <NavBar />
+      <Landing />
+    </Fragment>
+  );
+};
+
+export default App;
