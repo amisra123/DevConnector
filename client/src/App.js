2fccbd6a9dce73218e51a20f4944ed70f17f3609
amisra123 <aisuryamisra@gmail.com>
2021-07-22 20:36:56 +0530

    React frontend for register and signup integrated

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,32 @@
 import React, { Fragment } from 'react';
 import NavBar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
-import './App.css';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+//redux
+import { Provider } from 'react-redux';
+import store from './store';
 
+import './App.css';
 const App = () => {
   return (
-    <Fragment>
-      <NavBar />
-      <Landing />
-    </Fragment>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <NavBar />
+          <Route exact path='/' component={Landing} />
+          <section className='container'>
+            <Alert />
+            <Switch>
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/login' component={Login} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 };
 
